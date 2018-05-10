1bc002f4f3739041ac51b2a27421b509143414bc
howard <howarder3@gmail.com>
2018-05-10 23:37:52 +0800

    test2

--- app.py
+++ app.py
@@ -38,8 +38,12 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(
+    # message = TextSendMessage(text=event.message.text)
+    message = StickerSendMessage(
+    package_id='1',
+    sticker_id='1'
+	)
+	line_bot_api.reply_message(
         event.reply_token,
         message)
 
