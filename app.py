0aa53d0c461b7e60cc6f310a761d5b8eb11b0ed7
howard <howarder3@gmail.com>
2018-05-11 01:34:40 +0800

    test4

--- app.py
+++ app.py
@@ -38,8 +38,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print("event.reply_token:", event.reply_token)
-    print("event.message.text:", event.message.text)
+   	message = TextSendMessage(text='Hello, world')
+	line_bot_api.reply_message(event.reply_token, message)
+	# print("event.reply_token:", event.reply_token)
+    # print("event.message.text:", event.message.text)
     # if event.message.text == "eyny":
         # content = "eyny"
         # line_bot_api.reply_message(
@@ -48,8 +50,7 @@ def handle_message(event):
         # return 0
 	# else:
 		# usertext = usertext[::-1]
-	message = TextSendMessage(text='Hello, world')
-	line_bot_api.reply_message(event.reply_token, message)
+
 	
 import os
 if __name__ == "__main__":
