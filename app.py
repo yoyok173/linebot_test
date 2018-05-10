92650652231c2481fcf5cde2f498f261133c1ee3
howard <howarder3@gmail.com>
2018-05-11 01:18:48 +0800

    test10

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 	usertext = event.message.text
-	if (usertext == "abc"):
+	if (usertext == 'abc'):
         usertext = usertext[::-1]
 		message222 = TextSendMessage(text=usertext)		
 		line_bot_api.reply_message(event.reply_token, message222)		
