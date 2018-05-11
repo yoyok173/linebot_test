af315ec0fd34859a6cabdb0bd9cb24f645f997ff
howarder3 <howarder3@gmail.com>
2018-05-12 05:56:44 +0800

    add google sheet func.

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+import time
+import gspread
+import re
+import datetime
+
+from urllib.request import urlopen
+from oauth2client.service_account import ServiceAccountCredentials
+
 from flask import Flask, request, abort
 
 
@@ -21,7 +29,28 @@ line_bot_api = LineBotApi('+wjG+A6ltvlFVrmQmxyBaXcfljMtYaCTMXnVBoTxhWwMcSRX9+1mM
 handler = WebhookHandler('4c154ea12f7a284b5edd99087d760143')
 user_id = "Udf8f28a8b752786fa7a6be7d8c808ec6"
 
+auth_json_path = "./auth.json"
+gss_scopes = ['https://spreadsheets.google.com/feeds']
 
+gss_client = auth_gss_client(auth_json_path, gss_scopes)
+
+def update_sheet(gss_client, key, today,text):
+    wks = gss_client.open_by_key(key)
+    sheet = wks.sheet1
+    sheet.insert_row([today, text], 2)
+
+
+
+# update.py
+		
+#push message to one user
+# line_bot_api.push_message(user_id, 
+    # TextSendMessage(text='Hello World!'))
+# push message to multiple users
+# line_bot_api.multicast(['user_id1', 'user_id2'], 
+    # TextSendMessage(text='Hello World!'))	
+	
+	
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -53,7 +82,15 @@ def handle_message(event):
 		)
 	else:
 		message = TextSendMessage(text=event.message.text)
-		
+		spreadsheet_key = "19nQvlQIGRIoGELFxGfHWazG45DM7D2GccZg8wlD85_g"	
+		# spreadsheet_key_path = 'spreadsheet_key'
+		now = datetime.datetime.now()
+
+		# if cheapest_price is not None:
+		today = time.strftime("%c")
+		# with open(spreadsheet_key_path) as f:
+		#    spreadsheet_key = f.read().strip()
+		update_sheet(gss_client, spreadsheet_key, today, message)
 	#push message to one user
 	line_bot_api.push_message(user_id, 
 	TextSendMessage(text='Hello World!'))
@@ -62,14 +99,10 @@ def handle_message(event):
 		event.reply_token,
 		message)
 		
+def auth_gss_client(path, scopes):
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(path,scopes)
+    return gspread.authorize(credentials)
 
-		
-#push message to one user
-line_bot_api.push_message(user_id, 
-    TextSendMessage(text='Hello World!'))
-# push message to multiple users
-# line_bot_api.multicast(['user_id1', 'user_id2'], 
-    # TextSendMessage(text='Hello World!'))	
 		
 		
 import os
