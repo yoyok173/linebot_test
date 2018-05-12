4ad036bfa37043299e7e1196479eee81579477d8
howarder3 <howarder3@gmail.com>
2018-05-12 18:24:45 +0800

    fix

--- app.py
+++ app.py
@@ -117,10 +117,6 @@ today = time.strftime("%c")
 print(today)
 # print (event.source.userId)
 
-# line_bot_api.reply_message(event.reply_token,message)
-message = TextSendMessage(text='Hello World !!!')
-line_bot_api.push_message(user_id,TextSendMessage(text=message))
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 	print (event)	
