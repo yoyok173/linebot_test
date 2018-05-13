264381b2dd597b31cf10c3b69a9726ebbdfe8e0b
howarder3 <howarder3@gmail.com>
2018-05-13 15:30:50 +0800

    random reply

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def get_key_sheet(key):
 			list_response_index = [i for i,v in enumerate(list_key) if v==key]
 			print (list_response_index)
 			random_reply = random.randint(0,len(list_response_index)-1)
-			return list_response[random_reply+2]
+			return list_response[random_reply+1]
 		else:
 			return 0
 		
