6effc1737db6dd75fb55140a5ab9be566ede4e69
Howard Weng <howard@soslabde-MacBook-Pro.local>
2018-05-15 18:21:50 +0800

    fix

--- app.py
+++ app.py
@@ -538,7 +538,7 @@ def active_mode(user_message,event):
 		teach_result = teach(user_message,1)
 		message = TextSendMessage(text=teach_result)
 		line_bot_api.reply_message(event.reply_token,message)
-	elif(user_message.find("!智乃看圖片") == 0):
+	elif(user_message.find("!智乃看圖片","!給智乃看圖") == 0):
 		teach_result = teach_pic(user_message)
 		message = TextSendMessage(text=teach_result)
 		line_bot_api.reply_message(event.reply_token,message)
