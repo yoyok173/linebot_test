f540af10480b6536b2a717672ea68b1117d2bd86
Howard Weng <howard@soslabde-MacBook-Pro.local>
2018-05-15 20:58:33 +0800

    fix gmt

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ handler = WebhookHandler('4c154ea12f7a284b5edd99087d760143')
 auth_json_path = "./auth.json"
 
 now = datetime.datetime.now()
-today = time.strftime("%c")
+today = time.strftime(8,"%c")
 mode = 1
 
 # game SSR Prob
@@ -307,7 +307,7 @@ def leaderboard(key):
 	for i in range(0,10):
 		score_str += (str(list_top[i])+" --- "+list_score[i]+"\n【"+list_name[i]+"】\n")
 	# print(score_str)
-	score_str += str(time.strftime("%c"))
+	score_str += str(time.strftime(8,"%c"))
 	return score_str
 
 # def your_pants():
