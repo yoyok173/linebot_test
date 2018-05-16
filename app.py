8e3cbd0370e83f49ed6cf0bf715aeb74cc5a5543
Howard Weng <howard@soslabde-MacBook-Pro.local>
2018-05-16 12:12:14 +0800

    fix

--- app.py
+++ app.py
@@ -108,7 +108,9 @@ def get_score_sheet(list_top,list_name,list_target,target):
 			list_target.append(row[target])
 		
 def get_key_response(key):
-	global list_top,list_name,list_target
+	global list_key
+	global list_response
+	global list_type
 	if key in list_key:
 		# list_response = list_key.index(key)
 		list_response_index = [i for i,v in enumerate(list_key) if v==key]
@@ -135,7 +137,9 @@ gss_scopes = ['https://spreadsheets.google.com/feeds']
 gss_client = auth_gss_client(auth_json_path, gss_scopes)
 
 def update_sheet_key(gss_client, key, input , output):
-	global list_top,list_name,list_target
+	global list_key
+	global list_response
+	global list_type
 	wks = gss_client.open_by_key(key)
 	sheet = wks.sheet1
 	sheet.insert_row([input , output,"str"], 2)
@@ -144,7 +148,9 @@ def update_sheet_key(gss_client, key, input , output):
 	list_target.append("str")
 	
 def update_pic_sheet_key(gss_client, key, input , output):
-	global list_top,list_name,list_target
+	global list_key
+	global list_response
+	global list_type
 	wks = gss_client.open_by_key(key)
 	sheet = wks.sheet1
 	sheet.insert_row([input , output,"pic"], 2)
