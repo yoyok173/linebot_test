057f75dc0782dc15bfc9ebabab94043e31f290a2
Howard Weng <howard@soslabde-MacBook-Pro.local>
2018-05-16 10:54:37 +0800

    fix cmd box

--- app.py
+++ app.py
@@ -413,7 +413,7 @@ def forget(user_message):
 
 
 def search_cmd(user_message):
-	operations = [
+	operations_str = [
 	[["!閉嘴","!安靜","!你閉嘴","!你安靜"],switch_off()],
 	[["!說話"],switch_still_on()],
 	[["!使用說明書","!help","!說明書"],readme()],
@@ -427,29 +427,30 @@ def search_cmd(user_message):
 	[["活動進度"],event_progress()],
 	[["!抽食物","!食物"],get_food_sheet(1)],
 	[["!抽飲料","!飲料"],get_food_sheet(2)],
-	[["!cgss單抽"],"【CGSS 單抽結果：】\n" + gacha_CGSS()],
-	[["!cgss十連","!cgss十抽","!cgss10連","!cgss10抽"],"【CGSS 10連結果：】\n" + ten_gacha_CGSS()],
-	[["!bgd單抽","!gbp單抽"],"【BGD 單抽結果：】\n" + gacha_BGD()],
-	[["!bgd十連","!bgd十抽","!bgd10連","!bgd10抽","!gbp十連","!gbp十抽","!gbp10連","!gbp10抽"],"【BGD 10連結果：】\n" + ten_gacha_BGD()],
-	[["!sc單抽"],"【SC 單抽結果：】\n" + multi_gacha_SC(1)],
-	[["!sc十連","!sc十抽","!sc10連","!sc10抽"],"【SC 10連結果：】\n" + multi_gacha_SC(10)]
+	[["!cgss單抽"],"【CGSS 單抽結果】\n" + gacha_CGSS()],
+	[["!cgss十連","!cgss十抽","!cgss10連","!cgss10抽"],"【CGSS 10連結果】\n" + ten_gacha_CGSS()],
+	[["!bgd單抽","!gbp單抽"],"【BGD 單抽結果】\n" + gacha_BGD()],
+	[["!bgd十連","!bgd十抽","!bgd10連","!bgd10抽","!gbp十連","!gbp十抽","!gbp10連","!gbp10抽"],"【BGD 10連結果】\n" + ten_gacha_BGD()],
+	[["!sc單抽"],"【SC 單抽結果】\n" + multi_gacha_SC(1)],
+	[["!sc十連","!sc十抽","!sc10連","!sc10抽"],"【SC 10連結果】\n" + multi_gacha_SC(10)]
 	]
 
-	for i in range(len(operations)):
-		if user_message in operations[i][0]:
-			return operations[i][1] 
+
+	for i in range(len(operations_str)):
+		if user_message in operations_str[i][0]:
+			return TextSendMessage(text= operations_str[i][1])
+
 	return "not found in cmd list"
 
 
 def active_mode(user_message,event):
 	global mode
-	message = search_cmd(user_message)
-	if message != "not found in cmd list" :
-		line_bot_api.reply_message(event.reply_token,TextSendMessage(text=message))
+	message_get = search_cmd(user_message.lower())
+	if message_get != "not found in cmd list" :
+		line_bot_api.reply_message(event.reply_token,message_get)
 
 	if(user_message in ["貼圖辣","貼圖啦","貼圖","貼圖喇"]):
-		randsticker = random.randint(140,180)
-		message = StickerSendMessage(package_id='2',sticker_id=str(randsticker = random.randint(140,180)))
+		message = StickerSendMessage(package_id='2',sticker_id=str(random.randint(140,180)))
 		line_bot_api.reply_message(event.reply_token,message)
 	elif(user_message == "母湯電影版"):		
 		message = VideoSendMessage(
