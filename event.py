06864735a7c6452fb7ffd218b6ee64ebf05a4f3c
Howard Weng <howard@soslabde-MacBook-Pro.local>
2018-05-20 01:06:24 +0800

    fix food UI terminate games

--- event.py
+++ event.py
@@ -150,7 +150,7 @@ def fire_calculator(user_message):
 	except:
 		return "【請依照範例輸入：】\n!fire (剩餘火量) (活動％數,不用打％)"
 
-	message = "目前所剩石頭量:"+str(fire)+"\n可轉換分數:"+str(int(fire/3*(1+((score)/100))))
+	message = "目前所剩石頭量:"+str(fire)+"\n可轉換分數:"+str(int(int(fire)/3*(1+((score)/100))))
 	return message
 
 def stone_calculator(user_message):
@@ -167,7 +167,7 @@ def stone_calculator(user_message):
 		print('No data found.')
 	else:
 		for row in values:	
-			time = 120-row[0]
+			time = 120-float(row[0])
 	message = "活動剩餘時間:"+str(time)+"\n目前所剩石頭量:"+str(stone)+"\n全速到結束所需石頭:"+str(int(time*25*3*100/10))+"\n仍缺少石頭:"+str(int(time*25*3*100/10)-stone)+"\n剩餘石頭可轉換分數:"+str(int(stone/100*10/3*(1+((score)/100))))
 	return message
 
