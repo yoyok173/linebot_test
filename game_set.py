45da5d02235097df82e40a9341655ad61970ec6b
Howard Weng <howard@soslabde-MacBook-Pro.local>
2018-05-19 23:27:09 +0800

    fix food UI terminate games

--- game_set.py
+++ game_set.py
@@ -14,7 +14,7 @@ def is_number(s):
 def is_numberAB(user_guess):
 	try:
 		float(user_guess)
-		pattern = {[0-9]+{4}}
+		pattern = '[0-9]+{4}'
 		if re.match(patterm,user_guess):
 			return True
 		else:
